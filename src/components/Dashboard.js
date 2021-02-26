328b1169bb724460fd109b7ca201d08c0cfcb1fa
Deleon Francis <dxfrancis.coding@gmail.com>
2021-02-25 21:42:33 -0500

    added useMedia Query

---
+++ src/components/Dashboard.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { filterData, sortData } from "../util/util";
+import Jumbotron from "./Jumboton";
+import Table from "./Table";
+import MobileCards from "./MobileCards";
+
+export default function Dashboard({ employees }) {
+  // for responsiveness
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)",
+  });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+
+  // const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' })
+  // const isTabletOrMobileDevice = useMediaQuery({
+  //   query: '(max-device-width: 1224px)'
+  // })
+  // const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  // const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+
+  // internal state
+  const [employeeList, setEmployeeList] = useState(employees);
+
+  // Function that handles sorting
+  const sortEmployeesClick = (sortBy) => {
+    const employeesCopy = [...employees];
+    const sortedList = sortData(employeesCopy, sortBy);
+    setEmployeeList(sortedList);
+  };
+
+  // Function that handles the onFilterClick
+  const onFilterClick = (searchValue) => {
+    const employeesCopy = [...employees];
+    const filteredList = filterData(employeesCopy, searchValue);
+    setEmployeeList(filteredList);
+  };
+
+  // Function that handles the onClearClick
+  const onClearClick = () => {
+    setEmployeeList(employees);
+  };
+
+  return (
+    <div>
+      <Jumbotron onFilterClick={onFilterClick} onClearClick={onClearClick} />
+      <main className="d-flex justify-content-center">
+        <div class="container">
+          {isDesktopOrLaptop && (
+            <Table
+              sortEmployeesClick={sortEmployeesClick}
+              employeeList={employeeList}
+            />
+          )}
+          {isTabletOrMobile && (
+            <>
+              <MobileCards employeeList={employeeList} />
+            </>
+          )}
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,26 +1,45 @@
 import React from "react";
-import Modal from "./Modal";
+// import Modal from "./Modal";
 
 
 export default function Employee({ employee }) {
-
   return (
-    <tr>
-      <td className="align-middle">
-        <button type="button" class="btn btn-outline" data-toggle="modal" data-target="#exampleModalCenter">
+    <div className="row">
+      <div className="col">
+        <button
+          type="button"
+          className="btn btn-outline"
+          data-toggle="modal"
+          data-target="#exampleModalCenter"
+        >
           <img
             src={employee.picture}
             alt={`${employee.firstName} img`}
             className="img-thumbnail rounded-circle"
           />
         </button>
-        <Modal />
-      </td>
-
-      <td className="align-middle">{employee.firstName}</td>
-      <td className="align-middle">{employee.lastName}</td>
-      <td className="align-middle">{employee.email}</td>
-      <td className="align-middle">{employee.phone}</td>
-    </tr>
+      </div>
+      <div className="col center-text p-0">{employee.firstName}</div>
+      <div className="col center-text p-0">{employee.lastName}</div>
+      <div className="col center-text p-0">{employee.email}</div>
+      <div className="col center-text p-0">{employee.phone}</div>
+    </div>
   );
 }
+// <tr>
+//   <td className="align-middle">
+//     <button type="button" class="btn btn-outline" data-toggle="modal" data-target="#exampleModalCenter">
+//       <img
+//         src={employee.picture}
+//         alt={`${employee.firstName} img`}
+//         className="img-thumbnail rounded-circle"
+//       />
+//     </button>
+//     <Modal />
+//   </td>
+
+//   <td className="align-middle">{employee.firstName}</td>
+//   <td className="align-middle">{employee.lastName}</td>
+//   <td className="align-middle">{employee.email}</td>
+//   <td className="align-middle">{employee.phone}</td>
+// </tr>
